aef11e9a70df8525138cb039558336505dd58ee2
Duo Chai <duochai@amazon.com>
2023-10-19 10:59:41 -0400

    add Adapter and Bridge Pattern

---
+++ src/JavaPattern/AdapterPattern.java
@@ -0,0 +1,30 @@
+package JavaPattern;
+
+/**
+ * Adaptor is about provide consistent client interface by having an
+ * adapting logic in middle with the actual concrete implementation class
+ */
+public class AdapterPattern {
+    interface ClientInterface {
+        int criticalNumber();
+    }
+
+    class InternalClass {
+        int someNumber() {
+            return 100;
+        }
+    }
+
+    class InternalClassAdaptor implements ClientInterface {
+        private final InternalClass internalClass;
+        public InternalClassAdaptor(final InternalClass internalClass) {
+            this.internalClass = internalClass;
+        }
+
+        @Override
+        public int criticalNumber() {
+            final int someAdaptingNumber = 10_000;
+            return internalClass.someNumber() * someAdaptingNumber;
+        }
+    }
+}
---
+++ src/JavaPattern/BridgePattern.java
@@ -0,0 +1,58 @@
+package JavaPattern;
+
+/**
+ * Bridge is about delegation/reference-pointing, it serves as an abstraction and is provided to clients
+ * The point is multiple class might appear to be in a contain-relationship,
+ * instead of implementing inheritance, bridge pattern does the delegation work
+ */
+public class BridgePattern {
+    public interface Shape {
+        double getArea();
+    }
+
+    public interface Color {
+        String getColor();
+    }
+    /**
+     * Wrong pattern
+     */
+    public class RedSquare implements Shape, Color {
+        private final static String COLOR = "RED";
+
+        @Override
+        public double getArea() {
+            return 0;
+        }
+
+        public String getColor() {
+            return COLOR;
+        }
+    }
+
+    /**
+     * Right pattern
+     */
+    public class Red implements Color {
+        private final static String RED = "RED";
+
+        @Override
+        public String getColor() {
+            return RED;
+        }
+    }
+    public class Square implements Shape {
+        private Color color;
+        public Square(final Color color) {
+            this.color = color;
+        }
+
+        @Override
+        public double getArea() {
+            return 0;
+        }
+
+        public String getColor() {
+            return color.getColor();
+        }
+    }
+}
